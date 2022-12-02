b20616b49e3b6e6a26d99550df866f4d063cbb2f
ben <phas0ruk@gmail.com>
2022-12-02 11:43:46 -0800

    changefs config location for db

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
-const connectDB = require("../config/db");
+const connectDB = require("./config/db");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
