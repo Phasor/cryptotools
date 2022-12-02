1a4329a45aeebc39de27d0a7c48880dc7b656834
ben <phas0ruk@gmail.com>
2022-12-02 09:01:02 -0800

    updates db connection settings

--- config/db.js
+++ config/db.js
@@ -1,8 +1,32 @@
 const mongoose = require("mongoose");
 
+const devConnection = process.env.DB_STRING_DEV;
+const prodConnection = process.env.DB_STRING_PROD;
+
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
-  console.log(`Database connected: ${conn.connection.host}`);
+  if (process.env.NODE_ENV === "production") {
+    const db = mongoose.connect(prodConnection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    mongoose.connection.on("connected", () => {
+      console.log(
+        `Production database connected to ${mongoose.connection.host}`
+      );
+    });
+  } else if (process.env.NODE_ENV === "development") {
+    const db = mongoose.connect(devConnection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    mongoose.connection.on("connected", () => {
+      console.log(
+        `Development database connected to ${mongoose.connection.host}`
+      );
+    });
+  }
 };
 
 module.exports = connectDB;
