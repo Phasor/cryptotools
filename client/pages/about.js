ffca8072b2d5590d821cb0152592397d7c77b007
ben <phas0ruk@gmail.com>
2022-12-02 13:05:41 -0800

    adds next.js front end

---
+++ client/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function about() {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+        ducimus ipsam id suscipit nulla voluptates dolore voluptatibus impedit
+        perspiciatis, illo facilis ad dicta voluptatum sed magnam laborum eum!
+        Nostrum, voluptates.
+      </p>
+    </div>
+  );
+}
