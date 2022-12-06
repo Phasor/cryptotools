be7244381acbb8352e6eeb7e82eaeb5fce011f84
ben <phas0ruk@gmail.com>
2022-12-06 10:03:11 -0800

    hero design

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="flex justify-center items-center border border-yellow-700  bg-gradient-to-t from-white to-blue-200">
-      <div className="w-[50%] py-[250px] flex flex-col p-10 item ml-10">
+    <div className="absolute top-0 -z-50 w-screen flex justify-center items-center bg-gradient-to-t from-white to-blue-200">
+      <div className="w-[50%] py-[250px] px-20 flex flex-col item ml-10">
         <h1 className="text-7xl py-2 font-bold">Crypto Dashboards</h1>
         <h3 className="text-2xl py-2">Do Your Own Research.</h3>
         <p className="text-gray-500">
@@ -13,7 +13,7 @@ export default function Hero() {
         </p>
       </div>
       <div className="w-[50%] relative flex justify-center">
-        <Image src="/graph.png" width={500} height={500} />
+        <Image src="/data2.png" width={500} height={500} />
       </div>
     </div>
   );
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -2,10 +2,14 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav className="flex justify-end w-screen bg-yellow-600">
+    <nav className="flex justify-end w-screen p-2">
       <ul className="flex p-2 space-x-5 font-medium">
-        <li className="hover:border-b hover:border-black">About</li>
-        <li className="hover:border-b hover:border-black">Suggest a Link</li>
+        <li className="text-lg border border-gray-700 cursor-pointer py-2 px-3 rounded-sm hover:bg-gray-800 hover:bg-opacity-5">
+          About
+        </li>
+        <li className="text-lg border border-gray-700 cursor-pointer py-2 px-3 rounded-sm hover:bg-gray-800 hover:bg-opacity-5">
+          Suggest a Link
+        </li>
       </ul>
     </nav>
   );
