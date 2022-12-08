cdcef732ff70b0a602dd5a17aa9f0af7fe0bfed1
ben <phas0ruk@gmail.com>
2022-12-07 18:31:02 -0800

    adding authentication

--- client/components/ProjectRow.js
+++ client/components/ProjectRow.js
@@ -3,9 +3,13 @@ import React from "react";
 export default function ProjectRow({ project }) {
   return (
     <tr>
-      <td>{project.name}</td>
-      <td>{project.website}</td>
-      <td>{project.links.length}</td>
+      <td className="text-center p-3">
+        <img src={project.logo} alt="" />
+      </td>
+      <td className="text-right p-3">{project.symbol}</td>
+      <td className="text-right p-3">{project.name}</td>
+      <td className="text-right p-3">{project.website}</td>
+      <td className="text-right p-3">{project.links.length}</td>
     </tr>
   );
 }
--- client/components/Projects.js
+++ client/components/Projects.js
@@ -2,20 +2,24 @@ import React from "react";
 import ProjectRow from "./ProjectRow";
 
 const project = {
-  name: "Solana",
-  website: "www.solana.com",
+  name: "Bitcoin",
+  website: "www.bitcoin.com",
   links: ["www.link1.com", "www.link2.com", "www.link3.com"],
+  logo: "/images/btc.png",
+  symbol: "BTC",
 };
 
 export default function Projects() {
   return (
     <div className="absolute top-[600px]">
       <table>
-        <thead>
-          <tr>
-            <td>Project Name</td>
-            <td>Website</td>
-            <td># Links</td>
+        <thead className="border-t border-b border-gray-300 rounded">
+          <tr className="font-medium text-right">
+            <td className="p-3 text-center">Logo</td>
+            <td className="p-3">Symbol</td>
+            <td className="p-3">Project Name</td>
+            <td className="p-3">Website</td>
+            <td className="p-3"># Links</td>
           </tr>
         </thead>
         <tbody>
