9cb1190f4f2a9b6f3e62a30dc3bcbbb5d4858cdb
ben <phas0ruk@gmail.com>
2022-12-16 10:56:42 -0800

    styles admin edit project form

--- client/pages/project/[id].js
+++ client/pages/project/[id].js
@@ -101,81 +101,106 @@ export default function project() {
         <div className="h-screen w-screen bg-gray-100 flex justify-center">
           <form
             onSubmit={handleSubmit}
-            className="border shadow mt-10 p-6 w-[40%]"
+            className="border shadow mt-10 p-6 rounded-md w-full max-w-[400px] border-yellow-500 h-fit"
           >
             <h1 className="font-medium p-1 text-lg mb-3">
               Edit {data.project.name}
             </h1>
-            <div className="flex flex-col items-center space-y-6">
-              <label className="text-left">Name</label>
-              <input
-                type="text"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="rounded-md outline-none p-2 w-full"
-              />
-              <input
-                type="text"
-                value={symbol}
-                onChange={(e) => setSymbol(e.target.value)}
-                className="rounded-md outline-none p-2 w-full"
-              />
-              <input
-                type="text"
-                value={website}
-                onChange={(e) => setWebsite(e.target.value)}
-                className="rounded-md outline-none p-2 w-full"
-              />
 
-              {/* Picture Upload */}
-              <div onClick={() => imgInputRef.current.click()}>
-                <CameraIcon className="h-7 text-red-500" />
-                <p className="text-xs sm:text-sm xl:text-base">Picture</p>
+            <div className="flex flex-col">
+              <div className="my-2">
+                <label htmlFor="name" className="p-1 ">
+                  Name
+                </label>
                 <input
-                  ref={imgInputRef}
-                  className="hidden"
-                  id="inputTag"
-                  type="file"
-                  onChange={(e) => {
-                    addImageToPost(e);
-                    setImage(e.target.files[0]);
-                  }}
+                  type="text"
+                  name="name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="rounded-md outline-none p-2 w-full text-gray-500 mt-1 border-gray-500 shadow"
                 />
               </div>
-            </div>
+              <div className="my-2">
+                <label htmlFor="symbol" className="p-1 my-2">
+                  Symbol
+                </label>
+                <input
+                  type="text"
+                  name="symbol"
+                  value={symbol}
+                  onChange={(e) => setSymbol(e.target.value)}
+                  className="rounded-md outline-none p-2 w-full text-gray-500 mt-1 border-gray-500 shadow"
+                />
+              </div>
+              <div className="my-2">
+                <label htmlFor="website" className="p-1 my-2">
+                  Website
+                </label>
+                <input
+                  type="text"
+                  value={website}
+                  onChange={(e) => setWebsite(e.target.value)}
+                  className="rounded-md outline-none p-2 w-full text-gray-500 mt-1 border-gray-500 shadow"
+                />
+              </div>
 
-            {imgPreview && (
-              <div
-                onClick={removeImage}
-                className="flex flex-col text-center filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
-              >
-                <img
-                  src={imgPreview}
-                  alt="post preview"
-                  height={60}
-                  width={60}
-                  className="rounded-3xl overflow-hidden"
-                />
-                <p className="text-red-500 hover:text-red-700">Remove</p>
+              {/* Change logo section */}
+              <div className="flex space-x-5 items-center mt-3">
+                {/* Picture Upload */}
+                <div
+                  onClick={() => imgInputRef.current.click()}
+                  className="bg-blue-500 text-white rounded-md p-2 shadow my-3 w-[115px]"
+                >
+                  <p className="text-xs sm:text-sm xl:text-base">Change Logo</p>
+                  <input
+                    ref={imgInputRef}
+                    className="hidden"
+                    id="inputTag"
+                    type="file"
+                    onChange={(e) => {
+                      addImageToPost(e);
+                      setImage(e.target.files[0]);
+                    }}
+                  />
+                </div>
+
+                {/* Preview Image */}
+                {imgPreview && (
+                  <div
+                    onClick={removeImage}
+                    className="flex flex-col text-center filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer"
+                  >
+                    <img
+                      src={imgPreview}
+                      alt="post preview"
+                      height={60}
+                      width={60}
+                      className="rounded-3xl overflow-hidden"
+                    />
+                    <p className="text-red-500 hover:text-red-700">Remove</p>
+                  </div>
+                )}
               </div>
-            )}
-            {/* checkbox for active or not */}
-            <div className="flex items-center space-x-2 mt-5">
-              <p>Active</p>
-              <input
-                type="checkbox"
-                value={active}
-                onChange={(e) => setActive(e.target.value)}
-                className="rounded-md outline-none p-2 w-full"
-                defaultChecked={data.project.active}
-              />
+
+              {/* checkbox for active or not */}
+              <div className="flex items-center space-x-2 my-5">
+                <p className="mr-2">Active</p>
+                <input
+                  type="checkbox"
+                  name="active"
+                  value={active}
+                  onChange={(e) => setActive(e.target.value)}
+                  className="rounded-md outline-none p-2 h-[15px] w-[15px]"
+                  defaultChecked={data.project.active}
+                />
+              </div>
+              <button
+                onClick={handleSubmit}
+                className="bg-blue-500 hover:bg-blue-600 shadow-md text-white p-2 mt-3 rounded-md"
+              >
+                Update
+              </button>
             </div>
-            <button
-              onClick={handleSubmit}
-              className="bg-blue-500 text-white p-2 rounded-md"
-            >
-              Update
-            </button>
           </form>
         </div>
       )}
