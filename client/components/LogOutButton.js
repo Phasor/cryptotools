78befb7f65df87bb2e243510052e102eb1f29801
ben <phas0ruk@gmail.com>
2023-01-11 09:35:56 -0800

    adds log out button to admin page

---
+++ client/components/LogOutButton.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+
+export default function LogOutButton() {
+    const router = useRouter();
+
+    const LogOut = () => {
+        // remove token and user from local storage
+        localStorage.removeItem('token')
+        localStorage.removeItem('user')
+        // redirect to login page
+        router.push('/')
+    }
+
+  return (
+    <button
+    onClick={LogOut}
+    className="text-lg border border-gray-700 cursor-pointer py-2 px-3 rounded-sm hover:bg-gray-800 hover:bg-opacity-5"
+    >Log Out</button>
+  )
+}
--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
+import LogOutButton from "./LogOutButton";
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav className="absolute top-0 transparent flex justify-end w-screen p-2">
       <ul className="flex p-2 space-x-5 font-medium mr-5">
-        {router.pathname == "/" ? (
+        {router.pathname !== "/admin" ? (
           <>
             <Link href="/about">
               <li className="text-lg border border-gray-700 cursor-pointer py-2 px-3 rounded-sm hover:bg-gray-800 hover:bg-opacity-5">
@@ -27,6 +28,8 @@ export default function NavBar() {
                 Home
               </li>
             </Link>
+            <LogOutButton/>
+            
           </>
         )}
       </ul>
