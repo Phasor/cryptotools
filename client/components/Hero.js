ff8cb985ce7837ccefff5ab549e0c5aea4010048
ben <phas0ruk@gmail.com>
2023-01-12 10:29:38 -0800

    styling to hero component

--- client/components/Hero.js
+++ client/components/Hero.js
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="min-h-[500px] max-h-[600px] w-screen flex justify-center items-center bg-gradient-to-t from-[#F9F8F8] to-blue-200 overflow-x-hidden">
-      <div className="w-[50%] mt-[200px] px-20 flex flex-col item ml-20">
-        <h1 className="text-7xl py-2 font-bold ">Crypto Dashboards</h1>
+    <div className="lg:min-h-[500px] max-h-[600px] w-screen lg:flex justify-center items-center bg-gradient-to-t from-[#F9F8F8] to-blue-200">
+      <div className="p-5 lg:w-[50%] lg:mt-[200px] lg:px-20 flex flex-col lg:ml-20">
+        <h1 className="mt-10 text-7xl py-2 font-bold ">Crypto Dashboards</h1>
         <h3 className="text-2xl py-2">Do Your Own Research.</h3>
         <p className="text-gray-500">
           Links to great crypto data dashboards so you can stop relying on
           influncers and DYOR.
         </p>
       </div>
-      <div className="w-[50%] flex justify-center mr-20 mt-[200px]">
+      <div className="hidden lg:w-[50%] lg:flex lg:justify-center lg:mr-20 lg:mt-[200px]">
         <Image src="/data2.png" width={450} height={450} />
       </div>
     </div>
