a51f451cb35df71953645430a278a540198db1c2
ben <phas0ruk@gmail.com>
2023-01-13 13:58:52 -0800

    trying to deploy

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const passport = require("passport");
 const path = require("path");
 const compression = require("compression");
+const helmet = require("helmet");
 
 const app = express();
 
