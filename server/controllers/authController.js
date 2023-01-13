71a38f0e9d14f7d0c797e0fc1a8390d21e7e0052
ben <phas0ruk@gmail.com>
2023-01-13 10:28:17 -0800

    added request validation and sanitization to express rest api routes

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,45 +1,75 @@
 const { logUserIn } = require("../services/authService");
 const { verifyJWT, getUserIDFromToken } = require("../lib/utils");
+const { body, validationResult } = require("express-validator");
 
-exports.login = async (req, res) => {
-  try {
-    const user = await logUserIn(req.body.username, req.body.password);
-    console.log(`user: ${user}`);
-    if (user.success) {
+exports.login = [
+
+  // Validate and sanitize fields.
+  body("username", "Username must be specified.")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Username is not valid.")
+    .escape(),
+  body("password", "Password must not be empty.")
+    .trim()
+    .isAlphanumeric()
+    .isLength({ min: 4 })
+    .withMessage("Password must be at least 4 characters long.")
+    .escape(),
+
+    // process request after validation and sanitization
+
+  async (req, res) => {
+    try {
+      const user = await logUserIn(req.body.username, req.body.password);
+      // console.log(`user: ${user}`);
+      if (user.success) {
+        res.json({
+          success: true,
+          token: user.tokenObject.token,
+          expiresIn: user.tokenObject.expires,
+          user: user.user,
+        });
+      } else {
+        res.status(401).json({ success: false, message: user.message });
+      }
+    } catch (error) {
+      console.log(error);
       res.json({
-        success: true,
-        token: user.tokenObject.token,
-        expiresIn: user.tokenObject.expires,
-        user: user.user,
+        success: false,
+        message: `${error.name}, ${error.message}`,
       });
-    } else {
-      res.status(401).json({ success: false, message: user.message });
     }
-  } catch (error) {
-    console.log(error);
-    res.json({
-      success: false,
-      message: `${error.name}, ${error.message}`,
-    });
   }
-};
+];
 
 // function below checks if the user is logged in, checks the jwt is valid
-exports.isauth = async (req, res) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    // console.log(`token: ${token}`);
-    const verified = await verifyJWT(token);
-    // console.log(`verified: ${verified}`);
-    if (verified) {
-      res.json({success: true, user: verified.sub});
-      } else {
-        res.json({success: false, message: "Invalid token"});
+
+exports.isauth = [
+  // Validate and sanitize fields.
+  body("token", "Token must be specified.")
+    .trim()
+    .isLength({ min: 1 })
+    .withMessage("Token is not valid.")
+    .escape(),
+
+    // process request after validation and sanitization
+  async (req, res) => {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      // console.log(`token: ${token}`);
+      const verified = await verifyJWT(token);
+      // console.log(`verified: ${verified}`);
+      if (verified) {
+        res.json({success: true, user: verified.sub});
+        } else {
+          res.json({success: false, message: "Invalid token"});
+        }
+      } catch(error) {
+        console.log(error);
+        res.json({success: false, message: "Something went wrong"});
       }
-    } catch(error) {
-      console.log(error);
-      res.json({success: false, message: "Something went wrong"});
     }
-  };
+  ];
 
     
