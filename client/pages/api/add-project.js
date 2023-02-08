b545750f3c492d725d04c28c9d84b0fef5a069a6
ben <phas0ruk@gmail.com>
2023-02-08 12:22:35 -0800

    adds sanitisation and trimming to user input on add project page

--- client/pages/api/add-project.js
+++ client/pages/api/add-project.js
@@ -1,33 +1,6 @@
-// import Tool from '../../models/Tool';
-
-// export default async function handler(req, res) {
-//     //create a new Tool
-//     if (req.method === 'POST'){
-//         console.log(`Inside handler: ${JSON.stringify(req.body)}`);
-//         const tool = new Tool({
-//             name: req.body.name,
-//             image: req.body.image,
-//             website: req.body.website,
-//             shortDescription: req.body.shortDescription,
-//             longDescription: req.body.longDescription,
-//             active: req.body.active,
-//             category: req.body.category,
-//             rating: req.body.rating
-//         });
-//         try{
-//             const savedTool = await tool.save();
-//             res.status(200).json({ success: true, data: savedTool })
-//         } catch(err){
-//             res.status(500).json({ success: false, message: err.message })
-//         }
-//     } else {
-//         res.status(405).json({ success: false, message: 'Invalid request method' })
-//         res.end();
-//     }
-// }
-
 import Tool from '../../models/Tool';
 import { verifyJWT } from '../../utils/utils';
+const xss = require('xss');
 
 export default async function handler(req, res) {
     // check if they have sent a token
@@ -36,7 +9,6 @@ export default async function handler(req, res) {
     }
 
     if (req.method === 'POST'){
-        
         // verify the token
         const token = req.headers.authorization.split(' ')[1];
         try{
@@ -46,24 +18,34 @@ export default async function handler(req, res) {
         }
 
         try{
+            // sanitize and trim the data
+            const name = xss(req.body.name.trim());
+            const image = xss(req.body.image.trim());
+            const website = xss(req.body.website.trim());
+            const shortDescription = xss(req.body.shortDescription.trim());
+            const longDescription = xss(req.body.longDescription.trim());
+            const review = xss(req.body.review.trim());
+            const category = xss(req.body.category.trim());
+            const rating = xss(req.body.rating.trim());
+            const active = xss(req.body.active.trim());
+
             // create a new Tool
             const tool = new Tool({
-                name: req.body.name,
-                image: req.body.image,
-                website: req.body.website,
-                shortDescription: req.body.shortDescription,
-                longDescription: req.body.longDescription,
-                review: req.body.review,
-                active: req.body.active,
-                category: req.body.category,
-                rating: req.body.rating
+                name,
+                image,
+                website,
+                shortDescription,
+                longDescription,
+                review,
+                active,
+                category,
+                rating
             });
             const savedTool = await tool.save();
             return res.status(200).json({ success: true, data: savedTool })
         } catch(err){
             return res.status(500).json({ success: false, message: 'Server error' })
         }
-  
     } else {
         return res.status(405).json({ success: false, message: 'Invalid request method' })
     }
