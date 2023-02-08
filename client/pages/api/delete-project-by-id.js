562e89e12a12abae0f406e2b646bbfdcbff6d4d8
ben <phas0ruk@gmail.com>
2023-02-08 09:39:26 -0800

    adds authorization to delete project by id route

--- client/pages/api/delete-project-by-id.js
+++ client/pages/api/delete-project-by-id.js
@@ -1,21 +1,32 @@
 import Tool from '../../models/Tool';
+import { verifyJWT } from '../../utils/utils';
 
 export default async function handler(req, res) {
+    // check if they have sent a token
+    if (!req.headers.authorization) {
+        return res.status(401).json({ success: false, message: 'Unauthorized' })
+    }
+
     if (req.method === 'POST'){
         const toolId = req.body.id;
+        // verify the token
+        const token = req.headers.authorization.split(' ')[1];
         try{
-            Tool.findByIdAndDelete(toolId, (err, tool) => {
-                if(err){
-                    res.status(500).json({ success: false, message: 'Can not find project' })
-                }else{
-                    res.status(200).json({ success: true, data: tool })
-                }
-            })
+            const decodedToken = await verifyJWT(token);
+
         } catch(err){
-            res.status(500).json({ success: false, message: 'Server error' })
+            return res.status(401).json({ success: false, message: 'Unauthorized' })
         }
+
+        try{
+            await Tool.findByIdAndDelete(toolId)
+            return res.status(200).json({ success: true, message: 'Tool deleted successfully'})
+           
+        } catch(err){
+            return res.status(500).json({ success: false, message: 'Server error' })
+        }
+  
     } else {
-        res.status(405).json({ success: false, message: 'Invalid request method' })
-        res.end();
+        return res.status(405).json({ success: false, message: 'Invalid request method' })
     }
 }
