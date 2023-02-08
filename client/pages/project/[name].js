e318b590372efee6712e057df4cd47ae5c56225b
ben <phas0ruk@gmail.com>
2023-02-08 05:36:14 -0800

    cleans up code

--- client/pages/project/[name].js
+++ client/pages/project/[name].js
@@ -22,9 +22,6 @@ export default function Project() {
     enabled: name !== undefined,
   })
 
-  // if (projectyQuery.status === "loading") return <h1>Loading...</h1>
-  // if (projectyQuery.status === "error") return <h1>Error</h1>
-
   return (
     <div className='w-full min-h-screen bg-[#F9F8F8] overflow-y-auto relative'>
       <Script src="https://kit.fontawesome.com/b24cab7e32.js" crossorigin="anonymous"></Script>
@@ -94,78 +91,5 @@ export default function Project() {
       <Footer />
     </div>
   )
-
-  // return(
-  //   <>
-  //     <p>Data has loaded.</p>
-  //     <Image
-  //       src={projectyQuery.data.data[0].image}
-  //       alt={projectyQuery.data?.data?.name}
-  //       width={300}
-  //       height={300}
-  //       className='object-cover my-4 md:my-2 w-[150px] md:w-[300px]'
-  //     />
-  //   </>
-  // )
-
-  // { projectyQuery.data && (
-  
-  //     <div className='w-full min-h-screen bg-[#F9F8F8] overflow-y-auto relative'>
-
-  //       <Script src="https://kit.fontawesome.com/b24cab7e32.js" crossorigin="anonymous"></Script>
-  //       <Head>
-  //         <title>{projectyQuery.data?.data?.name}</title>
-  //         <meta
-  //           name="description"
-  //           content={`An overview of ${projectyQuery.data?.data?.name} crypto project.`}
-  //         />
-  //         <meta
-  //           name="keywords"
-  //           content="crypto dashboard, dashboard crypto, crypto data analytics, cryptocurrency research, blockchain, bitcoin, ethereum"
-  //         />
-  //         <link rel="icon" href="/favicon.ico" />
-  //       </Head>
-  //       <NavBar />
-  //       <Hero />
-  //       <div className='max-w-6xl mx-auto flex flex-col items-center justify-center sm:flex-row sm:space-x-8 p-4 overflow-y-auto mb-20'>
-  //             <Image
-  //               src={projectyQuery.data?.data?.image}
-  //               alt={projectyQuery.data?.data?.name}
-  //               width={300}
-  //               height={300}
-  //               className='object-cover my-4 md:my-2 w-[150px] md:w-[300px]'
-  //             />
-  //             <div className='flex flex-col border rounded-lg shadow-lg p-4'>
-  //               <h1 className='font-bold text-3xl'>{projectyQuery.data?.data?.name}</h1>
-  //               <p className='my-2'><span className='text-small px-2 bg-blue-500 text-white rounded-full'>{projectyQuery.data?.data?.category}</span></p>
-  //               <p className='my-2'><span className='font-semibold'>TLDR: </span>{projectyQuery.data?.data?.shortDescription}</p>
-  //               <h2 className='font-semibold mt-4'>Full Description</h2>
-  //               <p className='my-2'>{projectyQuery.data?.data?.longDescription}</p>
-  //               <span className='font-semibold'>Our Rating: 
-  //                 <Rating 
-  //                   className='text-yellow-500 ml-2'
-  //                   initialRating={parseInt(projectyQuery.data?.data?.rating)}
-  //                   readonly
-  //                   fullSymbol="fa fa-star"
-  //                   emptySymbol="fa fa-star-o"
-  //                   fractions={2}
-  //                 />
-  //                 </span>
-  //                 <div className='flex space-x-2 items-center mt-2'>
-  //                   <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="h-4 w-4"/>
-  //                   <a href={projectyQuery.data?.data?.website} target="_blank" className='text-blue-500 underline hover:text-blue-700'>Visit {projectyQuery.data.data.name}</a>
-  //                 </div>
-  //                   <Link href="/" className='mt-2 text-blue-500 underline hover:text-blue-700'>Back</Link>
-  //             </div>  
-  //       </div>
-  //       <Footer />
-
-  //       <p>FUCK THIS</p>
-  //     </div>
-  //   )
-  // }
-
-
-
 }
 
