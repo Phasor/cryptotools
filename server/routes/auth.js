8e06f4e9e211a466276af0db69e6f5fc0583ab8f
ben <phas0ruk@gmail.com>
2023-02-09 10:22:02 -0800

    removes old server folder

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-const passport = require("passport");
-
-router.post("/login", authController.login);
-router.post("/authcheck", authController.isauth);
-
-module.exports = router;
