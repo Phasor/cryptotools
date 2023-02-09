a38073e8aaf441125c6d625f602e2eb12aaea09b
ben <phas0ruk@gmail.com>
2023-02-09 12:37:24 -0800

    changes project card picture to be link to the project details page for the admin, to remove double nested link warning

--- client/components/ProjectCard.js
+++ client/components/ProjectCard.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Rating from "react-rating";
 import DeleteProjectButton from "./DeleteProjectButton";
 import EditProjectButton from "./EditProjectButton";
+import Link from "next/link";
 
 export default function ProjectCard({ project, isAdmin }) {
   let projectCategory = project.category;
@@ -13,13 +14,15 @@ export default function ProjectCard({ project, isAdmin }) {
     <div className="relative max-h-[400px]">
       <div className=" flex flex-col items-center justify-center sm:max-w-[300px] shadow-lg rounded-lg bg-white hover:bg-blue-100 cursor-pointer transform hover:scale-105">
         <div className="flex justify-center p-4">
-          <Image
-            src={project.image}
-            alt={project.name}
-            width={200}
-            height={200}
-            className="object-cover w-[150px] h-[150px] rounded-full "
-          />
+          <Link key={project._id} href={`/project/${project.name.split(" ").join("")}`}>
+            <Image
+              src={project.image}
+              alt={project.name}
+              width={200}
+              height={200}
+              className="object-cover w-[150px] h-[150px] rounded-full "
+            />
+          </Link>
         </div>
         <div className="flex flex-col items-start w-full p-4 overflow-hidden">
           <h1 className="text-xl font-bold">{project.name}</h1>
