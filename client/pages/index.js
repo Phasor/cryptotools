fd4f7e6c1c38e746fd7e3dda08b753fcc7758121
ben <phas0ruk@gmail.com>
2023-02-10 16:49:09 -0800

    removes server.js file for indes page, wip

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,27 +4,29 @@ import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import ProjectCard from "../components/ProjectCard";
-import { getActiveProjects } from "../queries/projectQueries";
+// import { getActiveProjects } from "../queries/projectQueries";
 import Script from "next/script";
 import SearchBox from "../components/SearchBox";
 import Link from "next/link";
 import EmailList from "../components/EmailList";
-import { useQuery } from "react-query";
+// import { useQuery } from "react-query";
 import Spinner from "../components/Spinner";
+import dbConnect from "../utils/dbConnect";
+import Tool from "../models/Tool";
 
-export default function Home() {
+export default function Home({ projects }) {
   const [searchValue, setSearchValue] = useState("");
   const [filteredProducts, setFilteredProducts] = useState([]);
 
-  const projectQuery = useQuery({
-    queryKey: "activeProjects",
-    queryFn: getActiveProjects,
-  });
+  // const projectQuery = useQuery({
+  //   queryKey: "activeProjects",
+  //   queryFn: getActiveProjects,
+  // });
 
   // Filter the products based on the search value
   useEffect(() => {
     setFilteredProducts(
-      projectQuery.data?.data.filter(
+      projects.filter(
         (product) =>
           product.name.toLowerCase().includes(searchValue.toLowerCase()) ||
           product.longDescription
@@ -36,7 +38,7 @@ export default function Home() {
           product.category.toLowerCase().includes(searchValue.toLowerCase())
       )
     );
-  }, [searchValue, projectQuery.data]);
+  }, [searchValue, projects]);
 
   return (
     <div className="w-screen min-h-screen relative bg-[#F9F8F8] overflow-y-auto">
@@ -62,32 +64,33 @@ export default function Home() {
       <SearchBox value={searchValue} setSearchValue={setSearchValue} />
 
       {/* Project List */}
-      {projectQuery.status === "success" && (
-        <main className="max-w-6xl p-4 md:p-0 items-center mx-auto mt-10 mb-20 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 lg:gap-10">
-          {filteredProducts?.map((project) => (
-            <Link
-              key={project._id}
-              href={`/project/${project.name.split(" ").join("")}`}
-            >
-              <ProjectCard key={project._id} project={project} />
-            </Link>
-          ))}
-        </main>
-      )}
-
-      {/* loading spinner */}
-      {projectQuery.status === "loading" && <Spinner />}
-
-      {/* error message */}
-      {projectQuery.status === "error" && (
-        <div className="text-center mt-10">
-          <h1 className="text-2xl font-bold">
-            Error: {projectQuery.error.message}
-          </h1>
-        </div>
-      )}
-
+      <main className="max-w-6xl p-4 md:p-0 items-center mx-auto mt-10 mb-20 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 lg:gap-10">
+        {filteredProducts?.map((project) => (
+          <Link
+            key={project._id}
+            href={`/project/${project.name.split(" ").join("")}`}
+          >
+            <ProjectCard key={project._id} project={project} />
+          </Link>
+        ))}
+      </main>
+      
       <Footer />
     </div>
   );
+}
+
+/* Retrieves pet(s) data from mongodb database */
+export async function getServerSideProps() {
+  await dbConnect()
+
+  /* find all the projects in our database */
+  const result = await Tool.find({ active: true });
+  const projects = result.map((doc) => {
+    const project = doc.toObject()
+    project._id = project._id.toString()
+    return project
+  })
+
+  return { props: { projects: projects } }
 }
