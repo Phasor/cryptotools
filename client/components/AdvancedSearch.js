e7feb0557ad2ee80e2730a3a9b3cf62143432475
ben <phas0ruk@gmail.com>
2023-02-16 03:55:41 -0800

    fixes advanced category search

--- client/components/AdvancedSearch.js
+++ client/components/AdvancedSearch.js
@@ -4,10 +4,11 @@ import Link from 'next/link'
 export default function AdvancedSearch() {
   const [categories, setCategories] = useState([])
   const [showCategories, setShowCategories] = useState(false)
+  const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL
 
   useEffect(() => {
     const fetchCategories = async () => {
-        const categories = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/get-all-categories`)
+        const categories = await fetch(`${BASE_URL}/api/get-all-categories`)
         const data = await categories.json()
         // console.log(data)
         setCategories(data.data)
@@ -28,8 +29,8 @@ export default function AdvancedSearch() {
             { showCategories && (
                 <div className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 gap-3 mt-2 border rounded-md p-2'>
                     {showCategories && categories && categories.map((category, index) => (
-                        <Link key={index} href={`/category/${category}`} className="text-blue-500 hover:text-blue-700 underline text-sm">
-                        {category.charAt(0).toUpperCase() + category.slice(1)}
+                        <Link key={index} href={`/category/${category.category}`} className="text-blue-500 hover:text-blue-700 underline text-sm">
+                        {category.category.charAt(0).toUpperCase() + category.category.slice(1)}
                     </Link>
                     ))}
                 </div>
--- client/pages/api/get-projects-by-category.js
+++ client/pages/api/get-projects-by-category.js
@@ -1,18 +1,23 @@
 const Tool = require("../../models/Tool");
+const Category = require("../../models/Category");
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
     try {
-      const projects = await Tool.find({ category: req.query.category });
+      // find the Category object ID of the category we are searching for
+      const category = await Category.findOne({category: req.query.category})
+      const categoryObjId = category._id
+      const projects = await Tool.find({ category: categoryObjId }).populate("category");
+
       // check if project exists
       if (projects.length === 0) {
         return res
           .status(404)
           .json({ success: false, message: "No projects found" });
       }
-      res.status(200).json({ success: true, data: projects });
+      return res.status(200).json({ success: true, data: projects });
     } catch (err) {
-      res.status(500).json({ success: false, message: err.message });
+      return res.status(500).json({ success: false, message: err.message });
     }
   } else {
     res.status(405).json({ success: false, message: "Invalid request method" });
